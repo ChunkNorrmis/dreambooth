933ad9f1349bcd733dacdc5ef1076b723bf4d619
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-02-10 17:42:38 -0500

    Update lsun.py

--- ldm/data/lsun.py
+++ ldm/data/lsun.py
@@ -2,35 +2,33 @@ import os
 import numpy as np
 import PIL
 from PIL import Image
+from PIL.ImageEnhance import Sharpness as sharpen
 from torch.utils.data import Dataset
 from torchvision import transforms
 from random import random, choice
 
 
+
 class LSUNBase(Dataset):
-    def __init__(
-        self,
-        txt_file,
-        data_root,
-        size=None,
-        interpolation="bicubic",
-        flip_p=0.5
-    ):
-        super().__init__()
+    def __init__(self,
+                 txt_file,
+                 data_root,
+                 size=None,
+                 interpolation="bicubic",
+                 flip_p=0.5
+                 ):
         self.data_paths = txt_file
         self.data_root = data_root
         with open(self.data_paths, "r") as f:
             self.image_paths = f.read().splitlines()
         self._length = len(self.image_paths)
-        self.size = size
-        self.odds = flip_p        
-        
         self.labels = {
             "relative_file_path_": [l for l in self.image_paths],
             "file_path_": [os.path.join(self.data_root, l)
                            for l in self.image_paths],
         }
 
+        self.size = size
         self.interp = {
             'linear': PIL.Image.LINEAR,
             "bilinear": PIL.Image.BILINEAR,
@@ -58,9 +56,9 @@ class LSUNBase(Dataset):
 
 
     def chance(self):
-        return f"{random():.2f}"
+        return f"{random():.2f}"    
 
-
+    
     def __getitem__(self, i):
         example = dict((k, self.labels[k][i]) for k in self.labels)
         image = Image.open(example["file_path_"])
