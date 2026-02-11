9b48d44342fa7fa5e6774d22195979bdf0b4bbbe
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-02-11 08:29:20 -0500

    Update lsun.py

--- ldm/data/lsun.py
+++ ldm/data/lsun.py
@@ -36,8 +36,7 @@ class LSUNBase(Dataset):
 
         self.transform = transforms.RandomChoice([
             transforms.RandomHorizontalFlip(p=1.0),
-            transforms.RandomPerspective(distortion_scale=0.5, p=1.0, interpolation=2, fill=0),
-            transforms.RandomRotation(90, interpolation=2, expand=True, center=None, fill=None)
+            transforms.RandomPerspective(distortion_scale=0.5, p=1.0, interpolation=2, fill=0)
         ])
 
 
