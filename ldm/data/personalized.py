6ee91b3d6e0f4d1016cbbb7709088ef314d3a4f3
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-02-10 20:26:12 -0500

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -1,16 +1,53 @@
 import os
-from typing import OrderedDict
 import numpy as np
 import PIL
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
-from captionizer import caption_from_path, generic_captions_from_path
-from captionizer import find_images
-from random import choice, random
-from PIL.ImageEnhance import Sharpness
 
+import random
 
+imagenet_templates_small = [
+    'a painting in the style of {}',
+    'a rendering in the style of {}',
+    'a cropped painting in the style of {}',
+    'the painting in the style of {}',
+    'a clean painting in the style of {}',
+    'a dirty painting in the style of {}',
+    'a dark painting in the style of {}',
+    'a picture in the style of {}',
+    'a cool painting in the style of {}',
+    'a close-up painting in the style of {}',
+    'a bright painting in the style of {}',
+    'a cropped painting in the style of {}',
+    'a good painting in the style of {}',
+    'a close-up painting in the style of {}',
+    'a rendition in the style of {}',
+    'a nice painting in the style of {}',
+    'a small painting in the style of {}',
+    'a weird painting in the style of {}',
+    'a large painting in the style of {}',
+]
+
+imagenet_dual_templates_small = [
+    'a painting in the style of {} with {}',
+    'a rendering in the style of {} with {}',
+    'a cropped painting in the style of {} with {}',
+    'the painting in the style of {} with {}',
+    'a clean painting in the style of {} with {}',
+    'a dirty painting in the style of {} with {}',
+    'a dark painting in the style of {} with {}',
+    'a cool painting in the style of {} with {}',
+    'a close-up painting in the style of {} with {}',
+    'a bright painting in the style of {} with {}',
+    'a cropped painting in the style of {} with {}',
+    'a good painting in the style of {} with {}',
+    'a painting of one {} in the style of {}',
+    'a nice painting in the style of {} with {}',
+    'a small painting in the style of {} with {}',
+    'a weird painting in the style of {} with {}',
+    'a large painting in the style of {} with {}',
+]
 
 per_img_token_list = [
     'א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת',
@@ -24,106 +61,75 @@ class PersonalizedBase(Dataset):
                  interpolation="bicubic",
                  flip_p=0.5,
                  set="train",
-                 placeholder_token="dog",
+                 placeholder_token="*",
                  per_image_tokens=False,
-                 center_crop=True,
-                 mixing_prob=0.25,
-                 coarse_class_text=None,
-                 token_only=False,
-                 reg=False
+                 center_crop=False,
                  ):
 
         self.data_root = data_root
 
-        self.image_paths = find_images(self.data_root)
+        self.image_paths = [os.path.join(self.data_root, file_path) for file_path in os.listdir(self.data_root)]
 
         # self._length = len(self.image_paths)
         self.num_images = len(self.image_paths)
-        self._length = self.num_images
+        self._length = self.num_images 
 
         self.placeholder_token = placeholder_token
-        self.token_only = token_only
+
         self.per_image_tokens = per_image_tokens
         self.center_crop = center_crop
-        self.mixing_prob = mixing_prob
-        self.odds = flip_p
-
-        self.coarse_class_text = coarse_class_text
 
         if per_image_tokens:
-            assert self.num_images < len(
-                per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
+            assert self.num_images < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
 
         if set == "train":
             self._length = self.num_images * repeats
 
         self.size = size
-        self.reg = reg
-        self.interp = {
-            'bilinear': PIL.Image.BILINEAR,
-            'bicubic': PIL.Image.BICUBIC,
-            'nearest': PIL.Image.NEAREST,
-            'lanczos': PIL.Image.LANCZOS
-        }[interpolation]
-
-        self.augment = {
-            'direction': {
-                'h_flip': 0,
-                'v_flip': 1,
-                '90_degree': 2,
-                '180_degree': 3,
-                '270_degree': 4
-            },
-            'clarity': {
-                'sharpen': random() + 1.0,
-                'blur': random() - 1.0
-            },
-        }
-
-        if self.reg and self.coarse_class_text:
-            self.reg_tokens = OrderedDict([('C', self.coarse_class_text)])
-
-
+        self.interpolation = {"linear": PIL.Image.LINEAR,
+                              "bilinear": PIL.Image.BILINEAR,
+                              "bicubic": PIL.Image.BICUBIC,
+                              "lanczos": PIL.Image.LANCZOS,
+                              }[interpolation]
+        
+        self.transform = tranforms.RandomChoice([
+            transforms.RandomHorizontalFlip(p=flip_p),
+            transforms.RandomPerspective(distortion_scale=0.5, p=flip_p,interpolation=2, fill=0),
+            transforms.RandomRotation(90, resample=False, expand=False, center=None, fill=None)
+        ])
+        
+    
     def __len__(self):
         return self._length
 
-
-    def chance(self):
-        return random()
-
-
     def __getitem__(self, i):
         example = {}
-        image_path = self.image_paths[i % self.num_images]
-        image = Image.open(image_path)
+        image = Image.open(self.image_paths[i % self.num_images])
 
         if not image.mode == "RGB":
             image = image.convert("RGB")
 
-        example["caption"] = ""
-        if self.reg and self.coarse_class_text:
-            example["caption"] = generic_captions_from_path(image_path, self.data_root, self.reg_tokens)
+        if self.per_image_tokens and np.random.uniform() < 0.25:
+            text = random.choice(imagenet_dual_templates_small).format(self.placeholder_token, per_img_token_list[i % self.num_images])
         else:
-            example["caption"] = caption_from_path(image_path, self.data_root, self.coarse_class_text, self.placeholder_token)
+            text = random.choice(imagenet_templates_small).format(self.placeholder_token)
+            
+        example["caption"] = text
 
-        if self.center_crop and image.width != image.height:
-            img = np.array(image).astype(np.uint8)
-            H, W = img.shape[0], img.shape[1]
-            _max = min(H, W)
-            img = img[(H - _max) // 2:(H + _max) // 2, (W - _max) // 2:(W + _max) // 2]
-            image = Image.fromarray(img)
-
-        if image.width > self.size or image.height > self.size:
-            image = image.resize((self.size, self.size), resample=self.interp, reducing_gap=3)
-
-        if self.chance() >= self.odds:
-            direction = choice(['h_flip', 'v_flip', '90_degree', '180_degree', '270_degree'])
-            image = image.transpose(self.augment['direction'][direction])
-                
-        if self.chance() >= self.odds:
-            clarity = choice(['sharpen', 'blur'])
-            image = sharpness(image).enhance(self.augment['clarity'][clarity])
-
+        # default to score-sde preprocessing
         img = np.array(image).astype(np.uint8)
-        example["image"] = (img / 127.5 - 1.0).astype(np.float32)
+        
+        if self.center_crop:
+            crop = min(img.shape[0], img.shape[1])
+            h, w, = img.shape[0], img.shape[1]
+            img = img[(h - crop) // 2:(h + crop) // 2,
+                (w - crop) // 2:(w + crop) // 2]
+
+        image = Image.fromarray(img)
+        if self.size is not None:
+            image = image.resize((self.size, self.size), resample=self.interpolation)
+
+        image = self.transform(image)
+        image = np.array(image).astype(np.uint8)
+        example["image"] = (image / 127.5 - 1.0).astype(np.float32)
         return example
