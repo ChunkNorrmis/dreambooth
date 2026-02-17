9976478945c4560c2fb26ae17812b60ad08dea06
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-02-17 03:15:28 -0500

    Refactor LSUNBase class initialization and methods

--- ldm/data/lsun.py
+++ ldm/data/lsun.py
@@ -1,42 +1,39 @@
 import os
 import numpy as np
-from PIL import Image, ImageEnhance
+import PIL
+from PIL import Image
+from PIL.ImageEnhance import Sharpness as Sharpen
 from torch.utils.data import Dataset
+from torchvision import transforms
 import random
 
 
 class LSUNBase(Dataset):
-    def __init__(
-        self,
-        txt_file,
-        data_root,
-        size=None,
-        interpolation="bicubic",
-        flip_p=0.5
-    ):
-
-        super().__init__()
+    def __init__(self,
+                 txt_file,
+                 data_root,
+                 size=None,
+                 interpolation="bicubic",
+                 flip_p=0.5
+                 ):
         self.data_paths = txt_file
         self.data_root = data_root
         with open(self.data_paths, "r") as f:
             self.image_paths = f.read().splitlines()
         self._length = len(self.image_paths)
-        self.flip_p = flip_p
-        self.size = size
-
         self.labels = {
             "relative_file_path_": [l for l in self.image_paths],
             "file_path_": [os.path.join(self.data_root, l)
                            for l in self.image_paths],
         }
 
-        self.interpolation = {
-            "linear": PIL.Image.LINEAR,
-            "bilinear": PIL.Image.BILINEAR,
-            "bicubic": PIL.Image.BICUBIC,
-            "lanczos": PIL.Image.LANCZOS,
-        }[interpolation]
-
+        self.size = size
+        self.interpolation = {"linear": PIL.Image.LINEAR,
+                              "bilinear": PIL.Image.BILINEAR,
+                              "bicubic": PIL.Image.BICUBIC,
+                              "lanczos": PIL.Image.LANCZOS,
+                              }[interpolation]
+        self.flip = flip_p
 
     def __len__(self):
         return self._length
@@ -49,25 +46,23 @@ class LSUNBase(Dataset):
 
         if self.center_crop and image.width != image.height:
             img = np.array(image).astype(np.uint8)
-            H, W = img.shape[0], img.shape[1]
-            crop = min(W, H)
-            img = img[(H - crop) // 2: (H + crop) // 2, (W - crop) // 2: (W + crop) // 2]
+            crop = min(img.shape[0], img.shape[1])
+            h, w, = img.shape[0], img.shape[1]
+            img =1 img[(h - crop) // 2:(h + crop) // 2,
+                      (w - crop) // 2:(w + crop) // 2]
             image = Image.fromarray(img)
 
-        if self.size is not None:
-            if image.width > self.size or image.height > self.size:
-                image = image.resize((self.size, self.size), resample=self.interpolation, reducing_gap=3)
+        if image.width > self.size or image.height > self.size:
+            image = image.resize((self.size, self.size), resample=self.interpolation, reducing_gap=3)
 
-        if random.random() < self.flip_p:
+        if self.flip >= random.random():
             image = random.choice([
-                image.transpose(method=random.randint(0, 4)),
-                ImageEnhance.Sharpness(image).enhance(random.uniform(-1.0, 2.0))
+                image.transpose(random.randrange(5)),
+                Sharpen(image).enhance(random.uniform(-0.5, 2.0))
             ])
-
-        img = np.array(image).astype(np.uint8)
-        img = (img / 127.5 - 1).astype(np.float32)
-        example['image'] = img
-
+            
+        image = np.array(image).astype(np.uint8)
+        example["image"] = (image / 127.5 - 1.0).astype(np.float32)
         return example
 
 
