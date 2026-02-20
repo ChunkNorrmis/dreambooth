951c5311ab3174584dd2a7e8029746b26838a583
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2026-02-20 05:27:37 -0500

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -82,24 +82,23 @@ class PersonalizedBase(Dataset):
             example["caption"] = caption_from_path(image_path, self.data_root, self.coarse_class_text, self.placeholder_token)
 
         if self.center_crop and image.width != image.height:
-            img = np.array(image).astype(np.uint8)
-            h, w = img.shape[0], img.shape[1]
+            image = np.array(image).astype(np.uint8)
+            h, w = image.shape[0], image.shape[1]
             crop = min(h, w)
-            img = img[(h - crop) // 2:(h + crop) // 2,
+            image = image[(h - crop) // 2:(h + crop) // 2,
                       (w - crop) // 2:(w + crop) // 2]
-            image = Image.fromarray(img)
+            image = Image.fromarray(image)
 
         if image.width != self.size or image.height != self.size:
             image = image.resize((self.size, self.size), resample=self.interpolation, reducing_gap=3)
 
         if self.chance > random.random():
             image = random.choice([
-                image.transpose(random.randrange(0, 2)),
-                image.transpose(random.randrange(2, 5)),
+                image.transpose(random.randrange(0, 5)),
                 Sharpen(image).enhance(random.uniform(0.5, 2.0))
             ])
             
         image = np.array(image).astype(np.uint8)
-        image = (image / 127.5 - 1.0).astype(np.float32)
-        example["image"] = image
+        img = (image / 127.5 - 1.0).astype(np.float32)
+        example["image"] = img
         return example
